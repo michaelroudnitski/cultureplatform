2e1e571ddf0d52d9f3fbdce089b948a9ebebea2f
michaelroudnitski <michaelroudnitski@gmail.com>
2019-05-02 14:41:08 -0400

    fixed route for login and added root users route

--- cultureplatform/users/views.py
+++ cultureplatform/users/views.py
@@ -2,4 +2,7 @@ from django.http import HttpResponse
 
 
 def index(request):
+    return HttpResponse("Hello World, you are viewing the user index page")
+
+def login(request):
     return HttpResponse("Hello, world. You're at the users index.")
