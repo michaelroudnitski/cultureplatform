fe011398524854947ab87b0dc3a80062df210cf6
ameenjaj <aljailaniamin@gmail.com>
2019-05-02 15:24:39 -0400

    Changed the path of some of the URLs

--- cultureplatform/users/views.py
+++ cultureplatform/users/views.py
@@ -3,6 +3,9 @@ from __future__ import unicode_literals
 
 from django.shortcuts import render
 from django.http import HttpResponse
+from django.contrib.auth import login, authenticate
+from django.contrib.auth.forms import UserCreationForm
+from django.shortcuts import render, redirect
 
 
 def login(request):
@@ -10,4 +13,16 @@ def login(request):
 
 
 def signup(request):
-    return HttpResponse("signup")
+    if request.method == 'POST':
+        form = UserCreationForm(request.POST)
+        if form.is_valid():
+            form.save()
+            username = form.cleaned_data.get('username')
+            raw_password = form.cleaned_data.get('password1')
+            user = authenticate(username=username, password=raw_password)
+            login(request, user)
+            return redirect('home')
+    else:
+        form = UserCreationForm()
+    return render(request, 'signup.html', {'form': form})
+
