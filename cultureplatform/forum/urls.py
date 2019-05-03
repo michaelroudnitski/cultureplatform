98f76fb8dea538cf82f2e4a548a7ae3777c34003
aljailaa <aljailaniamin@gmail.com>
2019-05-03 13:50:08 -0400

    added minor change

--- cultureplatform/forum/urls.py
+++ cultureplatform/forum/urls.py
@@ -5,9 +5,9 @@ from . import views
 urlpatterns = [
     url('create/', views.create, name='create'),
     url('/', views.forum, name='forum'),
-    url('/categories/culture_and_diversity/<int:id>', views.cultural_and_diversity, name='cul_and_diversity'),
-    url('/categories/philosophy_and_ethics/<int:id>', views.philosophy_and_ethics, name='philosophy_and_ethics'),
-    url('/categories/mental_health/<int:year>/', views.mental_health, name='mental_health'),
-    url('/categories/miscellaneous/<int:year>/', views.miscellaneous, name='miscellaneous'),
+    url('categories/culture_and_diversity/<int:id>', views.cultural_and_diversity, name='cul_and_diversity'),
+    url('categories/philosophy_and_ethics/<int:id>', views.philosophy_and_ethics, name='philosophy_and_ethics'),
+    url('categories/mental_health/<int:id>', views.mental_health, name='mental_health'),
+    url('categories/miscellaneous/<int:id>', views.miscellaneous, name='miscellaneous'),
 
 ]
