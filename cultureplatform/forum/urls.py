48fc9c0475b8e9d863156e717c556d90ab5334c7
aljailaa <aljailaniamin@gmail.com>
2019-05-03 14:49:01 -0400

    added minor change

--- cultureplatform/forum/urls.py
+++ cultureplatform/forum/urls.py
@@ -4,10 +4,10 @@ from . import views
 
 urlpatterns = [
     url('create/', views.create, name='create'),
-    url('/', views.forum, name='forum'),
-    url('categories/culture_and_diversity/<int:id>', views.cultural_and_diversity, name='cul_and_diversity'),
-    url('categories/philosophy_and_ethics/<int:id>', views.philosophy_and_ethics, name='philosophy_and_ethics'),
-    url('categories/mental_health/<int:id>', views.mental_health, name='mental_health'),
-    url('categories/miscellaneous/<int:id>', views.miscellaneous, name='miscellaneous'),
+    url('categories/culture_and_diversity', views.cultural_and_diversity, name='cul_and_diversity'),
+    url('categories/philosophy_and_ethics', views.philosophy_and_ethics, name='philosophy_and_ethics'),
+    url('categories/mental_health', views.mental_health, name='mental_health'),
+    url('categories/miscellaneous', views.miscellaneous, name='miscellaneous'),
+    url('', views.forum, name='forum'),
 
 ]
--- cultureplatform/forum/views.py
+++ cultureplatform/forum/views.py
@@ -5,6 +5,7 @@ from django.shortcuts import render
 from .forms import NewForumForm
 from django.http import HttpResponse
 
+
 def forum(request):
     return render(request, 'forum.html')
 
@@ -23,17 +24,17 @@ def create(request):
     return render(request, 'createforum.html', {'form': form})
 
 
-def cultural_and_diversity(request, id):
-    return HttpResponse("cultural diversity")
+def cultural_and_diversity(request):
+    return render(request, 'cultural_and_diversity.html')
 
 
-def philosophy_and_ethics(request, id):
-    return HttpResponse("philosophy_and_ethics")
+def philosophy_and_ethics(request):
+    return render(request, 'philosophy_and_ethics.html')
 
 
-def mental_health(request, id):
-    return HttpResponse("mental_health")
+def mental_health(request):
+    return render(request, 'mental_health.html')
 
 
-def miscellaneous(request, id):
-    return HttpResponse("miscellaneous")
+def miscellaneous(request):
+    return render(request, 'miscellaneous.html')
